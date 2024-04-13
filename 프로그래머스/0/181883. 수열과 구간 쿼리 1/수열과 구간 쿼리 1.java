98aaa8cf3805ccd264e4e22addf95f3c8f35bf00
KIMWOOBIN <109581515+KROOKIMWOOBIN@users.noreply.github.com>
2024-04-13 11:29:33 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.44 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,15 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = Arrays.copyOf(arr, arr.length);
         
-        for(int i = 0; i < queries.length; i++){
-            for(int j = queries[i][0]; j <= queries[i][queries[i].length - 1]; j++){
-                answer[j]++;
+        for(int[] querie : queries){
+            for(int i = querie[0]; i <= querie[1]; i++){
+                arr[i]++;
             }
         }
         
-        return answer;
+        return arr;
     }
 }
