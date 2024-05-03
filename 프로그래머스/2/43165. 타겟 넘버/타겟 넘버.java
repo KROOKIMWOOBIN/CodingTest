0e46cb38def8ecadf0ba712e0cc6f66020c696fa
KIMWOOBIN <109581515+KROOKIMWOOBIN@users.noreply.github.com>
2024-05-03 13:42:50 +0900

    [level 2] Title: 타겟 넘버, Time: 0.48 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = targetNumber(numbers, 0, target, 0);
+        return answer;
+    }
+    
+    public int targetNumber(int[] numbers, int depth, int target, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        int add = targetNumber(numbers, depth + 1, target, sum + numbers[depth]);
+        int sub = targetNumber(numbers, depth + 1, target, sum - numbers[depth]);
+        return add + sub;
+    }
+}
