06885088cce8ccb9f33d922bdfa5e3e9c553d084
KIMWOOBIN <109581515+KROOKIMWOOBIN@users.noreply.github.com>
2024-07-09 12:22:45 +0900

    [Bronze V] Title: A+B - 7, Time: 248 ms, Memory: 18636 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    private static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int T = sc.nextInt();
+        for (int i = 1; i <= T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            System.out.println("Case #" + i + ": " + (A + B));
+        }
+    }
+}
